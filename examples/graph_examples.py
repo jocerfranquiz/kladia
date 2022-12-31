bf0ebe04916c5491142b62ed75a256acd6056f45
jocerfranquiz <jocerfranquiz@gmail.com>
2022-12-31 12:48:43 -0500

    Update examples

--- examples/graph_examples.py
+++ examples/graph_examples.py
@@ -2,6 +2,8 @@
 from kladia.graph import graph
 
 if __name__ == '__main__':
+
+    # Create a simple 2 node graph
     g = graph()  # create an empty graph
     g.add(0, {'color': 'red'})  # add a node with a property
     g.add(1, {'color': 'blue'})  # add another node with a property
@@ -9,55 +11,60 @@ if __name__ == '__main__':
 
     print(g.to_dict())
 
-    # Test add_node
+    # Export a graph to a dictionary
     g1 = graph()
     g1.add(0, {'name': 'Node 0'})
     g1.add(2)
     print(f'g1: {g1.to_dict()}')
+
+    # Create a graph from a dictionary
     try:
         g2 = graph({'graph': {0: {3: {'label': 'node3'}}, 1: None, 3: None, 4: None}})
         print(f'g2: {g2.to_dict()}')
     except Exception as e:
         print(f'g2: {e}')
+
+    # Just remember that the graph must be a dictionary with a single key 'graph'
     try:
         g3 = graph({'grap': {0: None}})
     except Exception as e:
         print(f'g3 error: {e}')
 
-    # Test add for links
+    # Create a graph by adding links
     g4 = graph()
     g4.add((1, 2), {'label': 'link1'})
     g4.add((2, 3))
     g4.add((4, 4))
     print(f'g4: {g4.to_dict()}')
 
-    # Test delete_node
+    # Delete a node
     g5 = graph({'graph': {0: {3: {'label': 'node3'}}, 1: None, 3: None, 4: None}})
     g5.dlt(0)
     print(f'g5: {g5.to_dict()}')
 
-    # Test delete link
+    # Delete a link
     g6 = graph({'graph': {0: {3: {'label': 'node3'}}, 1: None, 3: None, 4: None}})
     g6.dlt((0, 3))
     print(f'g6: {g6.to_dict()}')
 
-    # Test nodes
-    g7 = graph({'graph': {0: {3: {'label': 'node3'}}, 1: None, 2: None, 3: None}})
-    print(f'g7 nodes: {g7.nodes()}')
-
-    # Test links
+    # Create a triangular graph
     triangular_graph = graph({'graph': {
         0: {1: {'label': 'link1'}},
         1: {2: {'label': 'link2'}},
         2: {0: {'label': 'link3'}},
     }})
 
-    # Test to_matrix
-    print(f'Triangular graph: {triangular_graph.to_dict()}')
+    # Get the nodes, links and adjacency matrix
     print(f'triangular_graph nodes: {triangular_graph.nodes()}')
     print(f'triangular_graph links: {triangular_graph.links()}')
     print(f'triangular_graph array: {triangular_graph.to_matrix()}')
 
-    # test empty graph to_matrix
-    g8 = graph()
-    print(f'g8 array: {g8.to_matrix()}')
+    # Create a graph from an adjacency matrix
+    matrix = [[0, 1, 0, 0, 0],  # link from node 0 to node 1
+              [0, 0, 1, 0, 0],  # link from node 1 to node 2
+              [1, 0, 0, 1, 0],  # links from node 2 to node 0 and node 3
+              [0, 0, 0, 0, 1],  # link from node 3 to node 4
+              [0, 0, 0, 0, 0]]  # node 4 has no links
+    g7 = graph()
+    g7.from_matrix(matrix)
+    print(f'g7: {g7.to_dict()}')
